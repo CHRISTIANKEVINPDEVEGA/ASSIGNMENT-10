2415b9cfba5f9ea6812b18ad924df7ee45785983
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2022-02-18 00:28:37 +0800

    Create a txt and qr code

--- QR_code.py
+++ QR_code.py
@@ -1,5 +1,11 @@
 import qrcode
+import cv2 
+
 txt=open("Contact_data.txt").read()
-img = qrcode.make(txt)
-type(img)  # qrcode.image.pil.PilImage
+img = qrcode.make(txt)  
 img.save("some_file.png")
+
+QR_val= cv2.QRCodeDetector()
+
+RetVal,Points,Straight_qrcode=QR_val.detectAndDecode(cv2.imread("some_file.png"))
+print(RetVal)
