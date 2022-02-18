92225c9f396ce5463cb148fded2b3e1f52efb72c
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2022-02-18 13:31:21 +0800

    basic reading of qr code

--- QR_code.py
+++ QR_code.py
@@ -1,11 +1,10 @@
+from asyncore import write
+from os import access
 import qrcode
 import cv2 
+import datetime
 
 txt=open("Contact_data.txt").read()
 img = qrcode.make(txt)  
 img.save("some_file.png")
-
-QR_val= cv2.QRCodeDetector()
-
-RetVal,Points,Straight_qrcode=QR_val.detectAndDecode(cv2.imread("some_file.png"))
-print(RetVal)
+ 
---
+++ QR_scanner.py
@@ -0,0 +1,25 @@
+from ast import While
+import cv2
+from pyzbar.pyzbar import decode
+
+webcam=cv2.VideoCapture(0)
+
+while True:
+    other, frame= webcam.read()
+
+    decoded_QR = decode(frame)  
+    
+    try:
+        print(decoded_QR[0][0])
+    except:
+        pass
+    cv2.imshow("QR_scanner.py", frame)
+
+    key=cv2.waitKey(1)
+
+    if key == 27:
+        break
+
+
+
+
