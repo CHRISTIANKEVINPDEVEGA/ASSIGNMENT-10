557b19441119470dbbf0904c810136a287fd0ed8
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2022-02-18 20:51:35 +0800

    cv2.rectangle was implemented, and for loop was used instead of the try/except func

--- QR_scanner.py
+++ QR_scanner.py
@@ -1,19 +1,23 @@
 from ast import While
 import cv2
 from pyzbar.pyzbar import decode
+from pyzbar import pyzbar
 import numpy as np
 
 
 def Scanning_func():
  webcam=cv2.VideoCapture(0)
+ webcam.set(3,640)
+ webcam.set(4,480)
  while True:
     other, frame= webcam.read()
-    decoded_QR = decode(frame)   
-    try:
-        string_decoded_QR=decoded_QR[0][0].decode("utf-8")
+    decoded_QR = pyzbar.decode(frame)   
+    for QR_code in decoded_QR:
+        string_decoded_QR = QR_code.data.decode("utf-8")
         print(string_decoded_QR)
-    except:
-        pass    
+        x_value, y_value , width, height = QR_code.rect
+        cv2.rectangle(frame, (x_value, y_value),(x_value+width, y_value+height), (300, 0, 280), 10)
+
     cv2.imshow("QR_scanner.py", frame)
     key=cv2.waitKey(1)
 
