0020faf41ac5230c21c77044a1e184e7d095c63b
CHRISTIAN KEVIN P. DE VEGA <christiandevega12@gmail.com>
2022-02-18 23:09:48 +0800

    the function was slpit into two, and some variables have been change

--- QR_code.py
+++ QR_code.py
@@ -1,16 +1,10 @@
 from asyncore import write
+from fileinput import close
 from os import access
 import qrcode
 import cv2 
-import datetime
-
-with open("Contact_data.txt") as lines:
-    data_per_line = lines.read()
-
-QR_img =qrcode.make(data_per_line)
-QR_img.save("My_QR.png")
 
 
-
-
-
+QR = open("Contact_data.txt","r").read()
+qr=qrcode.make(QR)
+qr.save("MY_QR.png")
--- QR_scanner.py
+++ QR_scanner.py
@@ -5,14 +5,8 @@ from pyzbar.pyzbar import decode
 from pyzbar import pyzbar
 import numpy as np
 import datetime
-
-def Scanning_func():
- webcam=cv2.VideoCapture(0)
- webcam.set(3,640)
- webcam.set(4,480)
- while True:
+def output_(Scanner_img_cap):
     DATE_TIME_saved = datetime.datetime.now()
-    other, Scanner_img_cap= webcam.read()
     decoded_QR = pyzbar.decode(Scanner_img_cap)   
     for QR_code in decoded_QR:
         string_decoded_QR = QR_code.data.decode("utf-8")
@@ -23,10 +17,21 @@ def Scanning_func():
         pts_for_letter = QR_code.rect
         cv2.putText(Scanner_img_cap, string_decoded_QR, (pts_for_letter[0],pts_for_letter[1]), cv2.FONT_ITALIC, 0.9 ,(250,2,59), 2)
         with open("Contact Tracing.txt", mode ='a') as contact_file_storage:
-            contact_file_storage.write("Contact have been saved: \n" + string_decoded_QR)
-            contact_file_storage.write("\n"+"\n"+"\n")
-            contact_file_storage.write(f"Contact was saved and scanned on:\n" + DATE_TIME_saved.strftime("%c"))
-    cv2.imshow("QR_scanner.py", Scanner_img_cap)
+            contact_file_storage.write("Contact have been saved:\n" + string_decoded_QR)
+            contact_file_storage.write("\n\n")
+            contact_file_storage.write(f"Contact was saved and scanned on:\n" + DATE_TIME_saved.strftime("%c")+"\n\n\n\n\n")
+            contact_file_storage.close    
+    return output_
+
+def Scanning_func():
+ webcam=cv2.VideoCapture(0)
+ webcam.set(3,640)
+ webcam.set(4,480)
+ while True:
+    other, Scanner_IMG_cap= webcam.read()
+    output_(Scanner_IMG_cap)                     
+    
+    cv2.imshow("QR_scanner.py", Scanner_IMG_cap)
     key=cv2.waitKey(1)
 
     if key == 27:
